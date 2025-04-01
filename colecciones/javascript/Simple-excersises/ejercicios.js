f2da7d5ce0ed3606cc39bb6bd0cffcdf6799410a
AlexisJG79 <alexisuriel.jimenezgarcia7@gmail.com>
2025-04-01 11:45:11 -0600

    first-commit

---
+++ colecciones/javascript/Simple-excersises/ejercicios.js
@@ -0,0 +1,30 @@
+const myName = 'Alexis';
+document.getElementById("myResult").innerHTML = myName
+
+let x=10;
+let y=5;
+let z=x*y;
+document.getElementById("area").innerHTML = z
+
+let r=2;
+let pi=3.1416;
+let res=pi*Math.pow(r,2);
+document.getElementById("area_circulo").innerHTML = res
+
+let b=8;
+let h=3;
+let area_tria=b*h/2;
+document.getElementById("area_triangulo").innerHTML = area_tria
+
+let rand=Math.random()*100;
+document.getElementById("numero_random").innerHTML = rand
+
+let rand_int=Math.floor(Math.random()*100);
+document.getElementById("numero_random_int").innerHTML = rand_int
+
+let base=Math.floor(Math.random()*50);
+let altura=Math.floor(Math.random()*100);
+let area_aleatoria=base*altura;
+document.getElementById("base_aleatoria").innerHTML = base
+document.getElementById("altura_aleatoria").innerHTML = altura
+document.getElementById("area_aleatoria").innerHTML = area_aleatoria
